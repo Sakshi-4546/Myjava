d59764401cc6a9d7e00842e4797d4dd3f0d86ece
Sakshi Jadhav <jadhavsakshi4546@gmail.com>
2025-12-23 22:28:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome!"); // This line prints the message
+        System.out.println("Welcome to my world!"); // This line prints the message
     }
 }
